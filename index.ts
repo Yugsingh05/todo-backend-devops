d044f661d9e374ad9977f4d8973d695949728148
Yugsingh05 <yugsingh7622@gmail.com>
2025-10-23 01:59:26 +0530

    feat fastify added

--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log("Hello via Bun!");
+import Fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import 'dotenv/config';
+
+const fastify = Fastify({ logger: true });
+
+fastify.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world 2' };
+});
+
+const start = async () => {
+  try {
+    const PORT = parseInt(process.env.PORT || '3000', 10);
+    await fastify.listen({ port: PORT, host: '0.0.0.0' });
+
+    const address = fastify.server.address();
+    if (typeof address === 'object' && address !== null) {
+      console.log(`🚀 Server listening on http://${address.address}:${address.port}`);
+    } else {
+      console.log(`🚀 Server listening`);
+    }
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
